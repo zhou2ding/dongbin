1205d882c80ab2508d337664f9c08aab8d02db91
zhoudongbin <458405826@qq.com>
2022-06-08 14:03:39 +0800

    【修改】补充cache的get和set

--- pkg/cache/redis.go
+++ pkg/cache/redis.go
@@ -6,7 +6,9 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"github.com/go-redis/redis"
+	"github.com/pkg/errors"
 	"go.uber.org/zap"
+	"time"
 )
 
 type Redis struct {
@@ -23,7 +25,7 @@ func InitRedis() (Cache, error) {
 		Password: pwd,
 	})
 	if _, err := cli.Ping().Result(); err != nil {
-		logger.GetLogger().Error("connect to redis host"+host+"failed", zap.Error(err))
+		logger.GetLogger().Error("connect to redis host failed", zap.String("host", host), zap.Error(err))
 		return nil, err
 	}
 
@@ -31,10 +33,60 @@ func InitRedis() (Cache, error) {
 }
 
 func (r *Redis) Set(key string, value interface{}, timeout int) error {
+	data, err := r.encode(value)
+	if err != nil {
+		return err
+	}
+	if r.cli == nil {
+		if err = r.reconnect(); err != nil {
+			return err
+		}
+	}
+
+	expire := time.Duration(timeout) * time.Second
+	if err = r.cli.Set(key, data, expire).Err(); err != nil {
+		logger.GetLogger().Error("set cache failed", zap.String("key", key), zap.Error(err))
+		return err
+	}
 	return nil
 }
 
 func (r *Redis) Get(key string, to interface{}, encode bool) error {
+	if r.cli == nil {
+		if err := r.reconnect(); err != nil {
+			return err
+		}
+	}
+
+	exist, err := r.Exists(key)
+	if err != nil {
+		logger.GetLogger().Error("get cache exists failed", zap.String("key", key), zap.Error(err))
+		return err
+	}
+	if !exist {
+		return errors.New("cache key not exist")
+	}
+
+	cmd := r.cli.Get(key)
+	if cmd == nil {
+		return errors.New("get cache no result")
+	}
+	if cmd.Err() != nil {
+		logger.GetLogger().Error("get cache failed", zap.Error(err))
+		return cmd.Err()
+	}
+
+	if encode {
+		if err = r.decode(cmd.Val(), to); err != nil {
+			logger.GetLogger().Error("get cache decode failed", zap.String("key", key), zap.Error(err))
+			return err
+		}
+	} else {
+		if err = json.Unmarshal([]byte(cmd.Val()), to); err != nil {
+			logger.GetLogger().Error("get cache unmarshal failed", zap.String("key", key), zap.Error(err))
+			return err
+		}
+	}
 	return nil
 }
 func (r *Redis) Del(key string) error {
