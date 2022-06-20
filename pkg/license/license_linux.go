9b9492a5d01a34f483a101ae3e9b01040c00d826
zhoudongbin <458405826@qq.com>
2022-06-20 14:40:15 +0800

    【新增】新增授权模块

---
+++ pkg/license/license_linux.go
@@ -0,0 +1,68 @@
+//go:build linux
+// +build linux
+
+package license
+
+/*
+#cgo CFLAGS: -I../../lib
+#cgo LDFLAGS: -L../../lib -lc_linux_x86_64_37124
+#include "c_api.h"
+#include <stdlib.h>
+*/
+import "C"
+import (
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"isvtsmp/pkg/traceutil"
+	"unsafe"
+)
+
+var vendorCode = "zhoudgonbin"
+
+const feature = 51
+
+type LicenseMgr struct {
+}
+
+var licenseMgr LicenseMgr
+
+func InitLicenseMgr(v *viper.Viper, logger *zap.Logger) error {
+	logger.Info("InitLicenseMgr linux")
+	return nil
+}
+
+func UnInitLicenseMgr(logger *zap.Logger) {
+	logger.Info("UnInitLicenseMgr linux")
+}
+
+func GetLicenseMgrInstance() *LicenseMgr {
+	return &licenseMgr
+}
+
+func (mgr *LicenseMgr) CheckValid() (bool, error) {
+	traceutil.GetLogger().Info("Check license linux...")
+	vcStr := C.CString(vendorCode)
+	defer C.free(unsafe.Pointer(vcStr))
+
+	var cHandle uint32
+	status := C.c_login(
+		feature,
+		C.c_vendor_code_t(unsafe.Pointer(vcStr)),
+		(*C.uint)(unsafe.Pointer(&cHandle)),
+	)
+	if status != 0 {
+		traceutil.GetLogger().Info("c_login failed")
+		return false, nil
+	}
+
+	status = C.c_logout(
+		C.uint(cHandle),
+	)
+	if status != 0 {
+		traceutil.GetLogger().Info("c_logout failed")
+	}
+
+	traceutil.GetLogger().Info("License is valid")
+
+	return true, nil
+}
---
+++ pkg/license/license_windows.go
@@ -0,0 +1,99 @@
+//go:build windows
+// +build windows
+
+package license
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+*/
+import "C"
+import (
+	"blog/pkg/logger"
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"sync"
+	"syscall"
+	"unsafe"
+)
+
+const vendorCode string = "zhoudongbin"
+
+const feature = 51
+
+type LicenseMgr struct {
+	libPath string
+}
+
+var licenseMgr LicenseMgr
+var once sync.Once
+
+func InitLicenseMgr(v *viper.Viper, logger *zap.Logger) error {
+	logger.Info("InitLicenseMgr windows")
+	return GetLicenseMgrInstance().init(v, logger)
+}
+
+func UnInitLicenseMgr(logger *zap.Logger) {
+	logger.Info("UnInitLicenseMgr windows")
+}
+
+func GetLicenseMgrInstance() *LicenseMgr {
+	return &licenseMgr
+}
+
+func (mgr *LicenseMgr) init(v *viper.Viper, logger *zap.Logger) error {
+	var err error = nil
+	once.Do(func() {
+		libPath := v.GetString("license.lib_path")
+		if len(libPath) == 0 {
+			logger.Info("lib path not set")
+			err = errors.New("lib path not set")
+		}
+		mgr.libPath = libPath
+	})
+
+	return err
+}
+
+func (mgr *LicenseMgr) CheckValid() (bool, error) {
+	logger.GetLogger().Info("Check license windows...")
+	handle, err := syscall.LoadDLL(mgr.libPath)
+	if err != nil {
+		return false, err
+	}
+
+	login, err := handle.FindProc("c_login")
+	if err != nil {
+		return false, err
+	}
+
+	logout, err := handle.FindProc("c_logout")
+	if err != nil {
+		return false, err
+	}
+
+	vcstr := C.CString(vendorCode)
+	defer C.free(unsafe.Pointer(vcstr))
+	var cHandle uint32
+	ret, _, lastErr := login.Call(feature, uintptr(unsafe.Pointer(vcstr)), uintptr(unsafe.Pointer(&cHandle)))
+	if lastErr != nil {
+		logger.GetLogger().Info("login.Call lastErr", zap.String("lastErr", lastErr.Error()))
+	}
+	if ret != 0 {
+		logger.GetLogger().Warn("login.Call failed")
+		return false, nil
+	}
+
+	ret, _, lastErr = logout.Call(uintptr(cHandle))
+	if lastErr != nil {
+		logger.GetLogger().Info("logout.Call lastErr", zap.String("lastErr", lastErr.Error()))
+	}
+	if ret != 0 {
+		logger.GetLogger().Warn("logout.Call failed")
+	}
+
+	logger.GetLogger().Info("License is valid")
+
+	return true, nil
+}
