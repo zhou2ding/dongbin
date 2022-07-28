db8d993b609790b3d820c4b3c954e8b6abdb16c1
zhoudongbin <458405826@qq.com>
2022-07-28 16:41:54 +0800

    【修改】补充mq：producer

--- pkg/mqbox/rabbitmq/producer.go
+++ pkg/mqbox/rabbitmq/producer.go
@@ -1,12 +1,26 @@
 package rabbitmq
 
-import "blog/pkg/mqbox"
+import (
+	"blog/pkg/mqbox"
+	"github.com/streadway/amqp"
+	"sync"
+)
 
 type RabbitProducer struct {
+	name          string
+	mtx           sync.RWMutex
+	conn          *amqp.Connection
+	ch            *amqp.Channel
+	exchangeBinds *mqbox.ExchangeBinds
+	close         chan *amqp.Error
+	status        uint8
 }
 
 func newRabbitProducer(name string, eb *mqbox.ExchangeBinds) *RabbitProducer {
 	return &RabbitProducer{
-
+		name:          name,
+		exchangeBinds: eb,
+		close:         make(chan *amqp.Error, 1),
+		status:        mqbox.StateClosed,
 	}
 }
