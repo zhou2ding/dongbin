bec4bbf3b334b1efadc8d74f9693c722dc2dd46a
zhoudongbin <458405826@qq.com>
2022-08-15 09:18:55 +0800

    【新增】新增监控系统资源并写入excel功能模块

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 )
 
 require (
+	github.com/360EntSecGroup-Skylar/excelize v1.4.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
@@ -23,9 +24,12 @@ require (
 	github.com/onsi/gomega v1.19.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/streadway/amqp v1.0.0
 	github.com/subosito/gotenv v1.3.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.10 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
---
+++ pkg/resourcemonitor/main.go
@@ -0,0 +1,129 @@
+package resourcemonitor
+
+import (
+	"fmt"
+	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/process"
+	"time"
+)
+
+const (
+	sheet1 = "Sheet1"
+)
+
+var (
+	titles = []string{
+		"Time",
+
+		"%Cpu",
+		"UsedMem",
+
+		// CPU百分比
+		"%Cpu_rabbitmq-server",
+		"%Cpu_redis-server",
+		"%Cpu_mysqld",
+		"%Cpu_mongod",
+
+		// 内存使用量
+		"Mem_rabbitmq-server",
+		"Mem_redis-server",
+		"Mem_mysqld",
+		"Mem_mongod",
+	}
+
+	services = map[string]int{
+		"rabbitmq-server": 1,
+		"redis-server":    2,
+		"mysqld":          3,
+		"mongod":          4,
+	}
+)
+
+type ResourceMonitor struct {
+	Times int
+}
+
+func (r *ResourceMonitor) Monitor() {
+	processList, _ := process.Processes()
+
+	excel := excelize.NewFile()
+	for i, v := range titles {
+		if 65+i > 90 {
+			excel.SetCellValue(sheet1, fmt.Sprintf("%c%c%d", 65, 39+i, 1), v)
+
+		} else {
+			excel.SetCellStr(sheet1, fmt.Sprintf("%c%d", 65+i, 1), v)
+		}
+	}
+
+	ticker := time.NewTicker(time.Second * 1)
+	defer ticker.Stop()
+	line := 2
+	for line < r.Times {
+		select {
+		case <-ticker.C:
+			// A列时间
+			now := time.Now()
+			excel.SetCellValue(sheet1, fmt.Sprintf("%v%d", "A", line), now.Format("2006-01-02 15:04:05"))
+
+			// B列CPU总占比
+			totalRate, err := getCpu()
+			if err != nil {
+				fmt.Println(err)
+			}
+			excel.SetCellValue(sheet1, fmt.Sprintf("%v%d", "B", line), totalRate)
+
+			// C列内存总使用量
+			usedMem, err := getMem()
+			if err != nil {
+				fmt.Println(err)
+			}
+			excel.SetCellValue(sheet1, fmt.Sprintf("%v%d", "C", line), usedMem)
+
+			// D-O列各服务CPU占比；P-AI列各服务内存使用量
+			for _, proc := range processList {
+				pName, _ := proc.Name()
+				if idx, ok := services[pName]; ok {
+					cpuRate, err := proc.CPUPercent()
+					if err != nil {
+						fmt.Println(err)
+					}
+					excel.SetCellValue(sheet1, fmt.Sprintf("%c%d", 67+idx, line), cpuRate)
+
+					memory, err := proc.MemoryInfo()
+					if err != nil {
+						fmt.Println(err)
+					}
+					if memory != nil {
+						if 79+idx > 90 {
+							// 超出Z列，从AA列开始
+							excel.SetCellValue(sheet1, fmt.Sprintf("%c%c%d", 65, 53+idx, line), memory.RSS/(1<<20))
+						} else {
+							excel.SetCellValue(sheet1, fmt.Sprintf("%c%d", 83+idx, line), memory.RSS/(1<<20))
+						}
+					}
+				}
+			}
+			line++
+		}
+	}
+	_ = excel.SaveAs("/home/user/gaowei/tsmp_resources/top" + time.Now().Format("20060102-15") + ".xlsx")
+}
+
+func getCpu() (float64, error) {
+	rates, err := cpu.Percent(time.Second*1, false)
+	if err != nil {
+		return 0, err
+	}
+	return rates[0], nil
+}
+
+func getMem() (uint64, error) {
+	vmem, err := mem.VirtualMemory()
+	if err != nil {
+		return 0, err
+	}
+	return vmem.Used / (1 << 20), err
+}
