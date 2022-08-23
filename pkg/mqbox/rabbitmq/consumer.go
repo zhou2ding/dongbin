b7a3855bf2de4dc08d0b8f8fd682a9d4bba67852
zhoudongbin <458405826@qq.com>
2022-08-23 11:29:15 +0800

    【修改】补充consumer open和reopen

--- pkg/mqbox/rabbitmq/consumer.go
+++ pkg/mqbox/rabbitmq/consumer.go
@@ -3,6 +3,7 @@ package rabbitmq
 import (
 	"blog/pkg/l"
 	"blog/pkg/mqbox"
+	"github.com/pkg/errors"
 	"github.com/streadway/amqp"
 	"go.uber.org/zap"
 	"sync"
@@ -18,6 +19,7 @@ type RabbitConsumer struct {
 	exchangeBinds *mqbox.ExchangeBinds
 	prefetch      int
 	closeCh       chan *amqp.Error
+	stopChan      chan struct{}
 	status        uint8
 }
 
@@ -39,6 +41,36 @@ func (r *RabbitConsumer) Topic() string {
 	return r.topic
 }
 
+func (r *RabbitConsumer) Open(mq interface{}) error {
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+
+	var ok bool
+	r.conn, ok = mq.(*amqp.Connection)
+	if !ok {
+		return errors.New("open param error")
+	}
+
+	var err error
+	r.ch, err = r.conn.Channel()
+	if err != nil {
+		l.GetLogger().Error("open channel error")
+		return err
+	}
+
+	if err = r.applyExchangeBinds(r.ch, r.exchangeBinds); err != nil {
+		_ = r.ch.Close()
+		return err
+	}
+	r.ch.NotifyClose(r.closeCh)
+
+	go r.keepalive()
+
+	r.status = mqbox.StateOpened
+	l.GetLogger().Info("rabbitmq open success", zap.String("name", r.name))
+	return nil
+}
+
 func (r *RabbitConsumer) Close() {
 	r.mtx.Lock()
 	r.ch.Close()
@@ -47,6 +79,57 @@ func (r *RabbitConsumer) Close() {
 }
 
 func (r *RabbitConsumer) Reopen(mq interface{}) error {
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+	if r.status == mqbox.StateOpened {
+		return nil
+	}
+
+	var ok bool
+	r.conn, ok = mq.(*amqp.Connection)
+	if !ok {
+		return errors.New("reopen param error")
+	}
+
+	channel, err := r.conn.Channel()
+	if err != nil {
+		return err
+	}
+	close(r.stopChan)
+	time.Sleep(time.Millisecond * 200)
+
+	r.stopChan = make(chan struct{})
+	r.closeCh = make(chan *amqp.Error, 1)
+	channel.NotifyClose(r.closeCh)
+	r.ch = channel
+
+	err = func(ch *amqp.Channel) error {
+		if err := r.applyExchangeBinds(ch, r.exchangeBinds); err != nil {
+			return err
+		}
+		if err := ch.Qos(r.prefetch, 0, false); err != nil {
+			return err
+		}
+		return nil
+	}(channel)
+
+	r.status = mqbox.StateOpened
+	l.GetLogger().Info("rabbitmq reopen success", zap.String("name", r.name))
+	return nil
+}
+
+func (r *RabbitConsumer) applyExchangeBinds(ch *amqp.Channel, binds *mqbox.ExchangeBinds) error {
+	if ch == nil || binds == nil {
+		return errors.New("channel or binds is nil")
+	}
+	if binds.Bindings == nil || binds.Exchange == nil {
+		return errors.New("bindings or exchange is nil")
+	}
+
+	ex := binds.Exchange
+	if err := ch.ExchangeDeclare(ex.Name, ex.Type, ex.Durable, ex.AutoDelete, ex.Internal, ex.NoWait, ex.Args); err != nil {
+		return err
+	}
 	return nil
 }
 
