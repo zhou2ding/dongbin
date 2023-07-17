6469fe420cbe177f94b66f9ce70cf2f05f4f1e91
zhoudongbin <458405826@qq.com>
2023-07-17 11:08:54 +0800

    fix:修改协程池实现

--- pkg/pool/pool.go
+++ pkg/pool/pool.go
@@ -1,7 +1,6 @@
 package pool
 
 import (
-	"blog/pkg/v"
 	"sync"
 )
 
@@ -17,7 +16,7 @@ type (
 		maxWorkers int
 		jobs       chan *Job
 		results    chan *Result
-		sync.WaitGroup
+		wg         sync.WaitGroup
 	}
 )
 
@@ -26,20 +25,10 @@ var (
 	once  sync.Once
 )
 
-func (p *Pool) Init() {
-	for i := 0; i < p.maxWorkers; i++ {
-		p.WaitGroup.Add(1)
-		go func(id int) {
-			defer p.WaitGroup.Done()
-			p.work(id)
-		}(i)
-	}
-}
-
-func GetPool() *Pool {
+func GetPool(maxWorkers int) *Pool {
 	once.Do(func() {
 		gPool = &Pool{
-			maxWorkers: v.GetViper().GetInt("pool.maxWorkers"),
+			maxWorkers: maxWorkers,
 			jobs:       make(chan *Job),
 			results:    make(chan *Result),
 		}
@@ -47,6 +36,42 @@ func GetPool() *Pool {
 	return gPool
 }
 
-func (p *Pool) work(id int) {
+func (p *Pool) Start() {
+	for i := 0; i < p.maxWorkers; i++ {
+		go p.worker()
+	}
+}
 
+func NewPool(maxWorkers int) *Pool {
+	return &Pool{
+		maxWorkers: maxWorkers,
+		jobs:       make(chan *Job),
+		results:    make(chan *Result),
+	}
+}
+
+func (p *Pool) Submit(job *Job) {
+	p.wg.Add(1)
+	p.jobs <- job
+}
+
+func (p *Pool) Wait() {
+	p.wg.Wait()
+	close(p.results)
+}
+
+func (p *Pool) worker() {
+	for job := range p.jobs {
+		sum := processJob(job)
+		result := &Result{
+			job: job,
+			sum: sum,
+		}
+		p.results <- result
+		p.wg.Done()
+	}
+}
+
+func processJob(job *Job) int {
+	return job.id * 2
 }
