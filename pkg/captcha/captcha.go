547569c2ac245b94724253fbb727e4c7f7f88996
zhoudongbin <458405826@qq.com>
2023-07-28 09:41:45 +0800

    fea：captcha

--- pkg/captcha/captcha.go
+++ pkg/captcha/captcha.go
@@ -1,1 +1,17 @@
 package captcha
+
+import (
+	"blog/pkg/v"
+	"github.com/mojocn/base64Captcha"
+)
+
+func GetCaptcha() (string, string, error) {
+	size := v.GetViper().GetIntSlice("captcha.size")
+	dr := base64Captcha.NewDriverDigit(size[0], size[1], size[2], 0.7, size[3])
+	capt := base64Captcha.NewCaptcha(dr, base64Captcha.DefaultMemStore)
+	id, b64, err := capt.Generate()
+	if err != nil {
+		return "", "", err
+	}
+	return id, b64, nil
+}
