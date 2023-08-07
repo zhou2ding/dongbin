bc54f3c66b72f226959f63e7f212984d636a4b97
zhoudongbin <458405826@qq.com>
2023-08-07 17:40:18 +0800

    fea：gotag，自增函数

--- utils/tags/fun/functions.go
+++ utils/tags/fun/functions.go
@@ -1,5 +1,7 @@
 package fun
 
+import "strconv"
+
 func Sum[T ZdbBaseType](p []T) T {
 	var sum T
 	for _, v := range p {
@@ -10,4 +12,25 @@ func Sum[T ZdbBaseType](p []T) T {
 
 func Const[T ZdbBaseType](p []T) T {
 	return p[0]
+}
+
+// IncrByN 形参的第一个元素为要执行增加的数字，第二个元素是该数字增加多少，第三个元素是已经加了几次
+func IncrByN[T ZdbNumber](p []T) T {
+	ret := p[0] + p[1]
+	if len(p) > 2 {
+		ret += p[2] * p[1]
+	}
+	return ret
+}
+
+// IncrByNStr 形参的第一个元素为要执行增加的数字，第二个元素是该数字增加多少，第三个元素是已经加了几次
+func IncrByNStr(p []string) string {
+	num, _ := strconv.Atoi(p[0])
+	step, _ := strconv.Atoi(p[1])
+	ret := num + step
+	if len(p) > 2 {
+		idx, _ := strconv.Atoi(p[2])
+		ret += idx * step
+	}
+	return strconv.Itoa(ret)
 }
